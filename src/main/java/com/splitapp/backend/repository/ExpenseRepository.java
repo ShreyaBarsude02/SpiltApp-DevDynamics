bda8826afc33a5330ee0758625f9153ebf2b651e
ShreyaBarsude02 <shreyabarsude@gmail.com>
2025-06-09 18:07:36 +0530

    added ExpenseRepository interface

---
+++ src/main/java/com/splitapp/backend/repository/ExpenseRepository.java
@@ -0,0 +1,10 @@
+package com.splitapp.backend.repository;
+
+import com.splitapp.backend.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    // later on
+}
